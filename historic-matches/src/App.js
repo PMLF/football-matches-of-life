55a8f4e47dc7bb922b31863592be735938e2aabd
PMLF <pmlfonseca@outlook.com>
2020-09-04 21:33:29 +0100

    Left sidebar for game selection has arrows.

--- historic-matches/src/App.js
+++ historic-matches/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.scss';
 import MatchThumbnail from './Components/MatchThumbnail/MatchThumbnail';
+import SidebarArrow from './Components/SidebarArrow/SidebarArrow';
 
 function App() {
   return (
     <div className="App">
 
       <div className="sidebar">
-        <MatchThumbnail/>
-        <MatchThumbnail/>
-        <MatchThumbnail/>
-        <MatchThumbnail/>
-        <MatchThumbnail/>
+        <SidebarArrow />
+        <div className="sidebar-main hideScrollBar">
+          <MatchThumbnail/>
+          <MatchThumbnail/>
+          <MatchThumbnail/>
+          <MatchThumbnail/>
+          <MatchThumbnail/>
+        </div>
+        <SidebarArrow />
       </div>
       <div className="main">
         a
