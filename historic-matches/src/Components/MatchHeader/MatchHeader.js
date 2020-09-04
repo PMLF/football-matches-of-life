6139c90934a48020336b458adda094c2a6816438
PMLF <pmlfonseca@outlook.com>
2020-09-04 23:02:44 +0100

    First draft for MatchHeader component complete.

--- historic-matches/src/Components/MatchHeader/MatchHeader.js
+++ historic-matches/src/Components/MatchHeader/MatchHeader.js
@@ -6,11 +6,11 @@ class MatchHeader extends Component {
     render() {
         return (
             <section className="MatchHeader">
-                <img src="" />
+                <img src="https://i.pinimg.com/originals/5a/93/25/5a932518772c4212677ed86ed9ea416d.png" alt="Home team logo" />
                 <div className="footage">
-
+                    <p>Images</p>
                 </div>
-                <img src="" />
+                <img src="https://upload.wikimedia.org/wikipedia/it/2/2d/Logo_FFF_%282018%29.png" alt="Away team logo" />
             </section>
         );
     }
