bc35b9204399b4707c7874c7061c0da43f1f2315
PMLF <pmlfonseca@outlook.com>
2020-09-24 00:13:39 +0100

    Centered club logos in MatchHeader.

--- historic-matches/src/Components/MatchHeader/MatchHeader.js
+++ historic-matches/src/Components/MatchHeader/MatchHeader.js
@@ -6,11 +6,15 @@ class MatchHeader extends Component {
     render() {
         return (
             <section className="MatchHeader">
-                <img src={this.props.data.teams[0].logo} alt="Home team logo" />
+                <div>
+                    <img src={this.props.data.teams[0].logo} alt="Home team logo" />
+                </div>
                 <div className="footage">
                     <p>Images</p>
                 </div>
-                <img src={this.props.data.teams[1].logo} alt="Away team logo" />
+                <div>
+                    <img src={this.props.data.teams[1].logo} alt="Away team logo" />
+                </div>
             </section>
         );
     }
