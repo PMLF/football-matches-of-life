456087be7a583c50914bac76f905133878b4aa0c
Pedro Fonseca <pmlfonseca@outlook.com>
2021-08-14 22:47:16 +0100

    Updates

---
+++ precache-manifest.28b0eddf96f7f66abc5942640d308c4f.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1877ba1db2481d10cabbead38113044",
+    "url": "/football-matches-of-life/index.html"
+  },
+  {
+    "revision": "a0d61fab7fe67f3d324b",
+    "url": "/football-matches-of-life/static/css/main.79ef4bce.chunk.css"
+  },
+  {
+    "revision": "27de3e9284a99b351734",
+    "url": "/football-matches-of-life/static/js/2.496e549a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/football-matches-of-life/static/js/2.496e549a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d61fab7fe67f3d324b",
+    "url": "/football-matches-of-life/static/js/main.6c849701.chunk.js"
+  },
+  {
+    "revision": "184ae76e0d9ecd2a123d",
+    "url": "/football-matches-of-life/static/js/runtime-main.9babbcc6.js"
+  },
+  {
+    "revision": "890a910cefcf7857fe6e1f2a6db27050",
+    "url": "/football-matches-of-life/static/media/academicaoaf.890a910c.png"
+  },
+  {
+    "revision": "bcfb5a1b64fa5253c6b7fac7a42100be",
+    "url": "/football-matches-of-life/static/media/aguero.bcfb5a1b.jpg"
+  },
+  {
+    "revision": "1fb59fa2a888e4405a10e4895b2e98d9",
+    "url": "/football-matches-of-life/static/media/assist.1fb59fa2.png"
+  },
+  {
+    "revision": "50ee4cb58a853b78b3a8360758e3078e",
+    "url": "/football-matches-of-life/static/media/aves.50ee4cb5.png"
+  },
+  {
+    "revision": "919f770efee3fd54e941cbdf3ae0c533",
+    "url": "/football-matches-of-life/static/media/beiramar.919f770e.png"
+  },
+  {
+    "revision": "cdb66ef9c95fc9084e21e3abed560c11",
+    "url": "/football-matches-of-life/static/media/benfica.cdb66ef9.png"
+  },
+  {
+    "revision": "d0d1df34e304fb6a05a9fe3f20d7a589",
+    "url": "/football-matches-of-life/static/media/bwinliga.d0d1df34.png"
+  },
+  {
+    "revision": "a48498b282db9654aa791cc8ff445ea7",
+    "url": "/football-matches-of-life/static/media/caldassc.a48498b2.png"
+  },
+  {
+    "revision": "ac1f9a4d3d4043f69dfe896e0ed595e5",
+    "url": "/football-matches-of-life/static/media/cardozo.ac1f9a4d.jpg"
+  },
+  {
+    "revision": "e8249f4013b0ac90ed5cec2ce6fa75b7",
+    "url": "/football-matches-of-life/static/media/chelsea.e8249f40.png"
+  },
+  {
+    "revision": "a1dee713b9b121e59c063ea4c1f5a1a7",
+    "url": "/football-matches-of-life/static/media/cisse.a1dee713.jpg"
+  },
+  {
+    "revision": "a1a497a87367c918e24fdacfee22f23f",
+    "url": "/football-matches-of-life/static/media/dzeko.a1a497a8.jpg"
+  },
+  {
+    "revision": "45bf1e3c730116519c6768299f13e568",
+    "url": "/football-matches-of-life/static/media/eder.45bf1e3c.jpg"
+  },
+  {
+    "revision": "6d653a1d06249a9f25bb9d675a8ec78d",
+    "url": "/football-matches-of-life/static/media/euro2016.6d653a1d.png"
+  },
+  {
+    "revision": "b977d5bb49d2d1dda5904f816b2d4226",
+    "url": "/football-matches-of-life/static/media/europaleague.b977d5bb.png"
+  },
+  {
+    "revision": "df9bf90564fbea5c93d9c1367cefe1d8",
+    "url": "/football-matches-of-life/static/media/fabioabreu.df9bf905.jpg"
+  },
+  {
+    "revision": "a0ab5ad047b0c3db08c1488b1e7ad9cd",
+    "url": "/football-matches-of-life/static/media/fcporto.a0ab5ad0.png"
+  },
+  {
+    "revision": "ffcfcd7b21a518f70e6d7e8891faf921",
+    "url": "/football-matches-of-life/static/media/fff.ffcfcd7b.png"
+  },
+  {
+    "revision": "8f5843c940b8d508bb5ecff6f031109b",
+    "url": "/football-matches-of-life/static/media/fpf.8f5843c9.png"
+  },
+  {
+    "revision": "afdc5f9fdfbf048eeca102681d1ba3ff",
+    "url": "/football-matches-of-life/static/media/goal.afdc5f9f.png"
+  },
+  {
+    "revision": "d35be0bb87a9b6153b2624fcbdc9820a",
+    "url": "/football-matches-of-life/static/media/ivanovic.d35be0bb.jpg"
+  },
+  {
+    "revision": "80f4df151f7c22cc0dcfa566922195e7",
+    "url": "/football-matches-of-life/static/media/jorgefellipe.80f4df15.jpg"
+  },
+  {
+    "revision": "f64d4b9130e14614faaa87cd39628545",
+    "url": "/football-matches-of-life/static/media/katsouranis.f64d4b91.jpg"
+  },
+  {
+    "revision": "e615312733773cfa6af7948caad06f5e",
+    "url": "/football-matches-of-life/static/media/kelvin.e6153127.jpg"
+  },
+  {
+    "revision": "9cb1ba7a667efafe281166593206a7be",
+    "url": "/football-matches-of-life/static/media/liganos.9cb1ba7a.png"
+  },
+  {
+    "revision": "518b61999da67749db183794d2ffa14c",
+    "url": "/football-matches-of-life/static/media/ligazonsagres.518b6199.png"
+  },
+  {
+    "revision": "37c70a1c74fc764301d062780471e5d5",
+    "url": "/football-matches-of-life/static/media/lima.37c70a1c.jpg"
+  },
+  {
+    "revision": "b9f323c7784883ce4468351dc04f2966",
+    "url": "/football-matches-of-life/static/media/limaolhanense1.b9f323c7.jpg"
+  },
+  {
+    "revision": "965c0abfdd1d0e141b3339212ef30017",
+    "url": "/football-matches-of-life/static/media/limaolhanense2.965c0abf.jpg"
+  },
+  {
+    "revision": "eae7d5262dddc5ae33f21c8694f41e20",
+    "url": "/football-matches-of-life/static/media/mackie.eae7d526.jpg"
+  },
+  {
+    "revision": "abe64d95790a6384e07a20fa30dcf425",
+    "url": "/football-matches-of-life/static/media/mancity2012.abe64d95.png"
+  },
+  {
+    "revision": "e05ef2ec216dd7adc2007e453d569829",
+    "url": "/football-matches-of-life/static/media/maxipereira.e05ef2ec.jpg"
+  },
+  {
+    "revision": "0953c9d6546c2d74bbf6352162f78f53",
+    "url": "/football-matches-of-life/static/media/moreirense.0953c9d6.png"
+  },
+  {
+    "revision": "e1c9e4748069d74911750ae80ae2ccb0",
+    "url": "/football-matches-of-life/static/media/nildopetrolina.e1c9e474.jpg"
+  },
+  {
+    "revision": "1e082222b7f8d1a8122e1ec70b7d1501",
+    "url": "/football-matches-of-life/static/media/olhanense.1e082222.png"
+  },
+  {
+    "revision": "c7b7c19ef9095c2fe72cd5441f1720d6",
+    "url": "/football-matches-of-life/static/media/owngoal.c7b7c19e.png"
+  },
+  {
+    "revision": "5a0226fd380b1ab8d9c1e6ebdee0766b",
+    "url": "/football-matches-of-life/static/media/penalty.5a0226fd.png"
+  },
+  {
+    "revision": "b4b0d76b601f634ee3149bbdee13a646",
+    "url": "/football-matches-of-life/static/media/petit.b4b0d76b.jpg"
+  },
+  {
+    "revision": "64b6a775b9d6af09766539b60a828ecf",
+    "url": "/football-matches-of-life/static/media/pizzi.64b6a775.jpg"
+  },
+  {
+    "revision": "76a4e65b2566eaac2703e3b8a53f27e5",
+    "url": "/football-matches-of-life/static/media/premierleague12.76a4e65b.png"
+  },
+  {
+    "revision": "f1a3fa2711ca729293842a5ba043ba0b",
+    "url": "/football-matches-of-life/static/media/qpr.f1a3fa27.png"
+  },
+  {
+    "revision": "c03cc977df2811370943e18757fa9308",
+    "url": "/football-matches-of-life/static/media/ricardo.c03cc977.png"
+  },
+  {
+    "revision": "3c8839cd5f2592c537c6724e3d6b9616",
+    "url": "/football-matches-of-life/static/media/tarzan.3c8839cd.jpg"
+  },
+  {
+    "revision": "8dd2d0da1b0760b88551ba4e19479cda",
+    "url": "/football-matches-of-life/static/media/taçaportugal.8dd2d0da.png"
+  },
+  {
+    "revision": "34ae7bb1c6f336ec80bde14658164ece",
+    "url": "/football-matches-of-life/static/media/torres.34ae7bb1.jpg"
+  },
+  {
+    "revision": "e902b405f7d416c5948d261a7ac073b0",
+    "url": "/football-matches-of-life/static/media/vitorgomes.e902b405.jpg"
+  },
+  {
+    "revision": "748341dedda915e8df951fbb13539470",
+    "url": "/football-matches-of-life/static/media/zabaleta.748341de.jpg"
+  }
+]);
